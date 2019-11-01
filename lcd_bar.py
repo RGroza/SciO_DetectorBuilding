ff002db4accac29cb45ee2ef2cc3e54cf57a8d41
Roberto Groza <robertogroza@gmail.com>
2019-11-01 10:26:33 -0400

    fixes

--- lcd_bar.py
+++ lcd_bar.py
@@ -4,55 +4,7 @@ import random
 import math
 
 fontdata1 = [
-        # char(0) - Left Bracket
-        [ 0b00111,
-          0b00100,
-          0b00100,
-          0b00100,
-          0b00100,
-          0b00100,
-          0b00111 ],
-
-        # char(1) - 1 unit
-        [ 0b10000,
-          0b10000,
-          0b10000,
-          0b10000,
-          0b10000,
-          0b10000,
-          0b10000 ],
-
-        # char(2) - 2 units
-        [ 0b11000,
-          0b11000,
-          0b11000,
-          0b11000,
-          0b11000,
-          0b11000,
-          0b11000,
-          0b11000 ],
-
-        # char(3) - 3 units
-        [ 0b11100,
-          0b11100,
-          0b11100,
-          0b11100,
-          0b11100,
-          0b11100,
-          0b11100,
-          0b11100 ],
-
-        # char(4) - 4 units
-        [ 0b11110,
-          0b11110,
-          0b11110,
-          0b11110,
-          0b11110,
-          0b11110,
-          0b11110,
-          0b11110 ],
-
-        # char(5) - 5 units
+        # char(0) - 5 units
         [ 0b11111,
           0b11111,
           0b11111,
@@ -62,7 +14,46 @@ fontdata1 = [
           0b11111,
           0b11111 ],
 
-        # char(6) - Empty Char
+        # char(1) - 4 units
+        [ 0b11110,
+          0b11110,
+          0b11110,
+          0b11110,
+          0b11110,
+          0b11110,
+          0b11110,
+          0b11110 ],
+
+        # char(2) - 3 units
+        [ 0b11100,
+          0b11100,
+          0b11100,
+          0b11100,
+          0b11100,
+          0b11100,
+          0b11100,
+          0b11100 ],
+
+        # char(3) - 2 units
+        [ 0b11000,
+          0b11000,
+          0b11000,
+          0b11000,
+          0b11000,
+          0b11000,
+          0b11000,
+          0b11000 ],
+
+        # char(4) - 1 unit
+        [ 0b10000,
+          0b10000,
+          0b10000,
+          0b10000,
+          0b10000,
+          0b10000,
+          0b10000 ],
+
+        # char(5) - Empty Char
         [ 0b00000,
           0b00000,
           0b00000,
@@ -71,6 +62,15 @@ fontdata1 = [
           0b00000,
           0b00000,
           0b00000 ],
+
+        # char(6) - Left Bracket
+        [ 0b00111,
+          0b00100,
+          0b00100,
+          0b00100,
+          0b00100,
+          0b00100,
+          0b00111 ],
 
         # char(7) - Right Bracket
         [ 0b11100,
@@ -100,23 +100,23 @@ try:
         bar_value = round(90*(random_value / value_range[1]))
         print(bar_value)
 
-        char_nums = [0, 0, 0, 0, 0, 0]
+        char_nums = [0, 0, 0, 0, 0]
 
         if bar_value % 5 != 0:
             char_nums[(bar_value % 5) - 1] = 1
         char_nums[4] = math.trunc(bar_value / 5)
 
         char_nums.reverse()
-        char_nums[5] = 18 - sum(char_nums)
+        char_nums.append() = 18 - sum(char_nums)
         print(char_nums)
 
         mylcd.lcd_write(0x80 + row_pos.get("3rd"))
 
-        mylcd.lcd_write_char(0) # Left Bracket
+        mylcd.lcd_write_char(6) # Left Bracket
         idx = 0
         for val in char_num:
             for n in range(val):
-                mylcd.lcd_write_char(5-idx)
+                mylcd.lcd_write_char(idx)
             idx += 1
         mylcd.lcd_write_char(7) # Right Bracket
 
